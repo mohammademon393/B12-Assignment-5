c6687c2136eeb86d003a062827d5eddbe879a37d
mohammademon393 <mdemon357ep@gmail.com>
2025-08-31 18:29:41 +0600

    clear button copy butto and more functionalities

--- js/script.js
+++ js/script.js
@@ -12,18 +12,20 @@ for(const heart of hearts){
 }
 
 
+
 //call-btn functionaltise
 let coins = 100;
 const coinDisplay = document.getElementById('coin');
 const callButton = document.getElementsByClassName('call-btn');
 const callHistory = document.getElementById('call-history');
 
-
 for( const btn of callButton){
     btn.addEventListener('click', function(){
         const card = btn.parentElement.parentElement;
         const serviceSubName = card.querySelector('.service-subName').innerText;
         const serviceNumber = card.querySelector('.service-number').innerText;
+        const name = card.querySelector('.name').innerText;
+
 
         //condistion
         if (coins < 20) {
@@ -31,12 +33,54 @@ for( const btn of callButton){
             return;
         }
         
-        //coins Reduceing kora
+        //coins Reduceing 
         coins -= 20;
         coinDisplay.innerText = coins;
         alert(`
              Calling ${serviceSubName} ${serviceNumber}
         `);
+
+        const time = new Date().toLocaleTimeString();
+            
+        //history section
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="bg-[#dfefff] h-[80px] rounded-lg flex justify-between items-center px-1 mt-2">
+                        <span>
+                            <h1 class="font-bold w-[70%]">${name}</h1>
+                            <p class="font-[400]">${serviceNumber}</p>
+                        </span>
+                        <span>
+                            <h3 class="font-semibold">${time}</h3>
+                        </span>
+                    </div>
+        `;
+        callHistory.appendChild(div);
     })
+}
+
+
+
+// clear button functionalitise
+const clear = document.getElementById('call-history');
+document.getElementById('clear-btn')
+    .addEventListener('click', function (){
+        clear.innerHTML = '';  
+});
+
+
+//copy btn functionalitis
+let copyNumber = 1;
+const btnCopy = document.getElementsByClassName('copy-btn');
+
+for(const copy of btnCopy){
+    copy.addEventListener('click', function(){
+        const copyCount = document.getElementById('copy-count');
+        const totalCopy = copyNumber++;
+
+        copyCount.innerText = totalCopy;
+    })
+
+
     
 }
